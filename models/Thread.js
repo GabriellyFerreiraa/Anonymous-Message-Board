0c04430694920441ccfa8d39f099a71f132f2cef
Gabrielly Ferreira <gabiferreira10@hotmail.com>
2025-12-06 14:41:57 -0300

    fix1

--- models/Thread.js
+++ models/Thread.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const ReplySchema = new new Schema({
+const ReplySchema = new Schema({
   text: { type: String, required: true },
   created_on: { type: Date, default: Date.now },
   delete_password: { type: String, required: true },
